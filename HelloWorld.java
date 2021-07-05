761fee6c7c70f56a3cf79cdd466bc76a054325df
Nilakshi-coder <55271036+Nilakshi-coder@users.noreply.github.com>
2021-07-05 11:57:02 +0530

    Updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello World");
     System.out.println("Welcome to GitHub!!!");
+
+    for(int i=0; i<n; i++){
+    	System.out.println(i);
+    }
   }
 }
