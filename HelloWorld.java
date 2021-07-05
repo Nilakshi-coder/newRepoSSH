99b52ef0f479a8e2e5bc2d65d653918c72431499
Nilakshi-coder <55271036+Nilakshi-coder@users.noreply.github.com>
2021-07-05 21:17:29 +0530

    Updated index.html with html tags

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld{
     System.out.println("Hello World");
     System.out.println("Welcome to GitHub!!!");
 
-    System.out.println("Even numbers are:");
+    System.out.println("Odd numbers are:"); // added at local
     for(int i=0; i<n; i++){
 	if(i%2==0)
     		System.out.println(i);
